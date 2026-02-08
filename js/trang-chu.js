07e5a5e36172fb3faabca1f119a3d0648a027f27
xuan-loc <Lannk2001@gmail.com>
2026-02-08 14:28:59 +0700

    them chat bot

--- js/trang-chu.js
+++ js/trang-chu.js
@@ -2,3 +2,7 @@ function toggleMenu() {
     const menu = document.getElementById("popularMenu");
     menu.style.display = (menu.style.display === "block") ? "none" : "block";
   }
+  function toggleChat() {
+    const chat = document.getElementById("chatbotBox");
+    chat.style.display = chat.style.display === "none" ? "block" : "none";
+}
