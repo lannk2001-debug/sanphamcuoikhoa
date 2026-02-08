bfae84b336e09b92f8c137f913e72287c477d4f1
xuan-loc <Lannk2001@gmail.com>
2026-02-08 14:53:31 +0700

    them cac chat bot vao

--- js/trang-chu.js
+++ js/trang-chu.js
@@ -2,7 +2,5 @@ function toggleMenu() {
     const menu = document.getElementById("popularMenu");
     menu.style.display = (menu.style.display === "block") ? "none" : "block";
   }
-  function toggleChat() {
-    const chat = document.getElementById("chatbotBox");
-    chat.style.display = chat.style.display === "none" ? "block" : "none";
-}
+
+(function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="sMhcNocsS5DzDHnC3u7hU";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
